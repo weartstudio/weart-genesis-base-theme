8792f8984faa81cf8c040497fcace01246cf29b6
weartstudio <ebalazs23@gmail.com>
2021-05-09 11:35:35 +0200

    save

--- front-page.php
+++ front-page.php
@@ -1,4 +1,7 @@
 <?php /* Home page */
 
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
 genesis(); ?>
